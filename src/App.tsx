163b578a12233dfaf879d416491d37af21deb8d2
Sinan Talha KOSAR <sinan@bayzat.com>
2024-08-09 13:32:55 +0300

    feat: Add rating form select component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import "./App.css";
-import { Form } from "react-final-form";
+import { Field, Form } from "react-final-form";
 import { ValidationErrors } from "final-form";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import FormHelperText from "@mui/material/FormHelperText";
+import Button from "@mui/material/Button";
 
 interface FormData {
   rating: "good" | "bad" | "neutral";
@@ -29,7 +35,41 @@ function App() {
       <Form<FormData>
         onSubmit={handleFormSubmit}
         validate={validate}
-        render={() => <></>}
+        render={({ handleSubmit }) => (
+          <div className="form">
+            <FormControl fullWidth>
+              <InputLabel id="rating-select-label">Rating</InputLabel>
+              <Field
+                name="rating"
+                render={({ input, meta }) => (
+                  <>
+                    <Select
+                      {...input}
+                      labelId="rating-select-label"
+                      label="Rating"
+                      error={meta.error && meta.touched}
+                      renderValue={(value) => (
+                        <div className="select-renderValue">{value}</div>
+                      )}
+                    >
+                      <MenuItem value="Good">Good</MenuItem>
+                      <MenuItem value="Neutral">Neutral</MenuItem>
+                      <MenuItem value="Bad">Bad</MenuItem>
+                    </Select>
+
+                    {meta.error && meta.touched && (
+                      <FormHelperText error={true}>{meta.error}</FormHelperText>
+                    )}
+                  </>
+                )}
+              />
+            </FormControl>
+
+            <Button variant="contained" onClick={handleSubmit}>
+              Submit
+            </Button>
+          </div>
+        )}
       />
     </>
   );
